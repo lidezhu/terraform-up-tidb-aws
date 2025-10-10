39ef6239897e2766497599743174ea2f3080ea2e
lidezhu <lidezhu@pingcap.com>
2025-10-10 15:02:59 +0800

    update config

--- locals_advanced.tf
+++ locals_advanced.tf
@@ -6,15 +6,15 @@ locals {
   image  = "ami-0fb653ca2d3203ac1" # Ubuntu 20.04
 
   # If you want to change instance type, ensure that GP3 EBS is available in the instance type.
-  tidb_instance    = "c5.4xlarge"
+  tidb_instance    = "c5.9xlarge"
   tikv_instance    = "r5.2xlarge"
-  pd_instance      = "c5.9xlarge"
-  tiflash_instance = "c5.9xlarge"
-  ticdc_instance   = "c5.12xlarge"
+  pd_instance      = "c5.4xlarge"
+  tiflash_instance = "c5.4xlarge"
+  ticdc_instance   = "c5a.8xlarge"
   center_instance  = "c5.2xlarge"
 
   # TODO: deploy a load balancer for downstream
-  tidb_downstream_instance    = "c5a.16xlarge"
+  tidb_downstream_instance    = "c5.9xlarge"
 
   master_ssh_key         = "./master_key"
   master_ssh_public      = "./master_key.pub"
--- locals_common.tf
+++ locals_common.tf
@@ -1,10 +1,10 @@
 locals {
   name      = "cdc-test"
-  n_tidb    = 5
-  n_tikv    = 24
-  n_ticdc   = 2
+  n_tidb    = 3
+  n_tikv    = 15
+  n_ticdc   = 12
   n_tiflash = 0
 
-  n_tidb_downstream = 0
-  n_tikv_downstream = 0
+  n_tidb_downstream = 1
+  n_tikv_downstream = 10
 }
