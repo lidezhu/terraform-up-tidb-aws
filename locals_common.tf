fddeabab345b8e9b21310cef9b4e3ccf42cdea2f
lidezhu <lidezhu@pingcap.com>
2026-04-10 16:25:37 +0800

    update

--- locals_common.tf
+++ locals_common.tf
@@ -1,10 +1,10 @@
 locals {
   name      = "cdc-test"
   n_tidb    = 3
-  n_tikv    = 15
-  n_ticdc   = 12
+  n_tikv    = 6
+  n_ticdc   = 3
   n_tiflash = 0
 
-  n_tidb_downstream = 1
-  n_tikv_downstream = 10
+  n_tidb_downstream = 0
+  n_tikv_downstream = 0
 }
